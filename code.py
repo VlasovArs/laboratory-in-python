7d4452f1b50f6353dd14023231e45609aae53d84
VlasovArs <89856595+VlasovArs@users.noreply.github.com>
2022-09-23 23:04:37 +0300

    Delete code.py

--- code.py
+++
@@ -1,9 +0,0 @@
-a = [int(input()) for k in range(10)]    #Генератор массива а
-i=int(input())    #1 индекс
-j=int(input())    #2 индекс
-if a[i]*a[j]>0:
-    a[i],a[j]=a[j],a[i]
-else:
-    for m in range(i+1,j):    #Все элементы между i и j
-        a[m]=0
-print(a)
